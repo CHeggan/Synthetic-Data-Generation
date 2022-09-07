c4aacdb746ec0fa70014a02c0fe94e45851aa780
CHeggan <calumheggan2@hotmail.com>
2022-09-07 15:17:13 +0100

    Update function_gens.py

--- function_gens.py
+++ function_gens.py
@@ -1,7 +1,11 @@
 """
 Contains the base functions needed to generate data. Each function takes a set of 
     operating parameters defined elsewhere and a noise parameter which is used to 
-    generate a additive noise ona  sample-wise basis 
+    generate a additive noise ona  sample-wise basis.
+
+We have:
+    - different variant for each function
+    - a random noise addition to y values
 """
 
 ################################################################################
@@ -19,9 +23,15 @@ import numpy as np
 ###########################
 # 1D
 ###########################
-def random_exp(x_space, variant='all'):
+def exp_1d(x_space, n_min, n_max, variant='any'):
     # variant: exp(x^n), exp (x*n), exp(x/n)
+    if variant == 'any':
+        variant = random.choice(['exponent', 'mult'])
+        sample, variant, n = random_exp_sample()
+
     
 ###########################
 # 2D
 ###########################
+def exp_2d(x_space, ):
+    pass
