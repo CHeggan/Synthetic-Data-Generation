bbefba8e916fa6167b4001300ac6e8ee1461c232
Calum Heggan <s1529508@ed.ac.uk>
2022-10-09 18:35:19 +0100

    Update MatrixGenerator.py

--- Matrix Inversion/MatrixGenerator.py
+++ Matrix Inversion/MatrixGenerator.py
@@ -16,15 +16,24 @@ class MatrixGenerator():
         self.data_type = data_type
 
     def create_random_matrix(self):
+        # Create matrix with only ints
         if self.data_type == 'int':
-
             matrix = torch.randint(low=int(self.low), 
                 high=int(self.high),
-                size=(self.size, self.size))
+                size=(self.size, self.size)).to(dtype=float)
+        
+        # Create matrix with only floats
+        elif self.data_type == 'float':
+            matrix = (self.low-self.high)*torch.randn(
+                size=(self.size, self.size)) + self.high
 
-        print(matrix)
-        print(matrix.shape)
+        return matrix
 
 
-gen = MatrixGenerator(3, 0, 10, 'int')
+    def calc_determinant(self, matrix):
+        det = torch.linalg.det(matrix)
+        return det
+
+
+gen = MatrixGenerator(2, 0, 10, 'int')
 gen.create_random_matrix()
